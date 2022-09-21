05b177097a229ca1aef728073fbb374176645142
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-20 22:59:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pprint
 import torch
 from pythonbasictools import DeepLib, log_device_setup, logs_file_setup
 
-from applications.heidelberg.results_generation import train_with_params
+from results_generation import train_with_params
 from neurotorch.modules.layers import LayerType
 
 if __name__ == '__main__':
-	logs_file_setup(__file__)
+	logs_file_setup(__file__, add_stdout=False)
 	log_device_setup(deepLib=DeepLib.Pytorch)
-	torch.autograd.set_detect_anomaly(True)
-	torch.cuda.set_per_process_memory_fraction(0.1)
+	if torch.cuda.is_available():
+		torch.cuda.set_per_process_memory_fraction(0.8)
 	results = train_with_params(
 		{
 			"use_recurrent_connection": True,
@@ -25,8 +25,9 @@ if __name__ == '__main__':
 		batch_size=256,
 		verbose=True,
 		show_training=False,
-		force_overwrite=True,
-		data_folder="tr_test",
+		force_overwrite=False,
+		data_folder="data/tr_test",
+		pin_memory=True,
 	)
 	pprint.pprint(results, indent=4)
 	results["history"].plot(show=True)
